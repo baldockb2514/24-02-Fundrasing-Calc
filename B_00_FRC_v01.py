802d648a32f475a94920022a82c58f5cfdbb4e48
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2024-02-19 14:58:31 +1300

    Base File

---
+++ B_00_FRC_v01.py
@@ -0,0 +1,18 @@
+# Functions go here
+
+# Check that an int/float is more than 0
+def num_check(question, error, num_type):
+    while True:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+                continue
+
+            return response
+
+        except ValueError:
+            print(error)
+            continue
