69037bd8a05a9097eb3e9c7d8a45ab6e80a8cd66
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2024-03-01 11:05:56 +1300

    Print instructions

---
+++ C_09_instructions.py
@@ -0,0 +1,25 @@
+# Shows instructions
+def show_instructions():
+    print('''\n 
+***** Instructions *****
+
+This Program will ask you for...
+- The name of the product that you are selling
+- How many items you plan on selling
+- The costs for each component of the product
+- How much money you want to make
+
+When you have entered all the items, press 'xxx' to quit.
+
+The program will then display an itemised list of the costs
+ with subtotals for the variable and fixed costs.
+Finally it will tell you how much you should sell
+ each item for to reach your profit goal.
+
+The data will also be automatically written to a text file which
+ has the same name as your product
+
+**************************''')
+
+
+show_instructions()
